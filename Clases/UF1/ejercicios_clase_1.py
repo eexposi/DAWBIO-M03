f2b54625bc8c1157fbdc567be1eb99346269e3f8
Éric <eexposi1@xtec.cat>
2022-09-15 13:15:44 +0200

    Ej1

--- Clases/UF1/ejercicios_clase_1.py
+++ Clases/UF1/ejercicios_clase_1.py
@@ -10,8 +10,20 @@ Mostraremos por pantalla con un print() la información completa, indicando que 
 Crearemos una variable en la que concatenaremos la informacion e indicaciones de cada elemento. Mostrandola
 ______________________________
 """
-
-
+# PEDIMOS LA INFORMACION  AL USUARIO
+# nombre: str = input("Indique su nombre: ")
+# apellido: str = input("Indique su apellido: ")
+# edad: int = int(input("Indique su edad: "))
+# grupo: str = input("Indique su grupo: ")
+# asignatura: str = input("Indique su asignatura: ")
+#
+# print("Mostramos la informacion en un print:")
+# print("Me llamo " + nombre + " " + apellido + ", tengo " + str(edad) + " años. Estoy cursando "
+#       + asignatura + " en el grupo" + grupo + ".")
+# resultado = "Me llamo " + nombre + " " + apellido + ", tengo " + str(edad) + " años. Estoy cursando " \
+#             + asignatura + " en el grupo" + grupo + "."
+# print("Mostramos la informacion desde la variable")
+# print(resultado)
 
 """
 EJERCICIO 2
@@ -20,3 +32,4 @@ Realizaremos las operaciones: +, -, *, /, % y guardaremos el resultado en variab
 Printaremos el resultado y la operacion de cada una de ellas
 ______________________________
 """
+n1: int = int(input("Numero 1: "))
