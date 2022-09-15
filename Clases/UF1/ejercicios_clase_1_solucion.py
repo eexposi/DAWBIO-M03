6abbd0f7e8853acd5478e204492b94a16b198ee2
Éric <eexposi1@xtec.cat>
2022-09-15 13:22:00 +0200

    Todo resuelto

--- Clases/UF1/ejercicios_clase_1_solucion.py
+++ Clases/UF1/ejercicios_clase_1_solucion.py
@@ -32,4 +32,19 @@ Realizaremos las operaciones: +, -, *, /, % y guardaremos el resultado en variab
 Printaremos el resultado y la operacion de cada una de ellas
 ______________________________
 """
+
+# Pedimos la información
 n1: int = int(input("Numero 1: "))
+n2: int = int(input("Numero 2: "))
+
+suma: int = n1 + n2
+resta: int = n1 - n2
+division: int = n1 / n2
+multi: int = n1 * n2
+modulo: int = n1 % n2
+
+print("El resultado de la suma de " + str(n1) + " y de " + str(n2) + " es de: " + str(suma))
+print("El resultado de la resta de " + str(n1) + " y de " + str(n2) + " es de: " + str(resta))
+print("El resultado de la division de " + str(n1) + " y de " + str(n2) + " es de: " + str(division))
+print("El resultado de la multiplicacion de " + str(n1) + " y de " + str(n2) + " es de: " + str(multi))
+print("El resultado del modulo de " + str(n1) + " y de " + str(n2) + " es de: " + str(modulo))
