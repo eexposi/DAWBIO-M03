b89c558e7c87c84cd69e62ab1d22e49ff70b5364
ericexposito <eexposi1@xtec.cat>
2022-09-16 19:18:51 +0200

    ejercicios_clase_2.py resueltos

--- Clases/UF1/ejercicios_clase_2.py
+++ Clases/UF1/ejercicios_clase_2.py
@@ -9,8 +9,22 @@ Las printaremos todas separandolas por "---" entre ellas en un solo print. Y fin
 Solicitaremos al usuario una palabra y un número.
 Repetiremos en una variable la palabra tantas veces como diga la constante.
 """
-
-
+# import array
+#
+# texto: str = "Hola soy un texto"
+# numero: int = 100
+# decimal: float = 2.5
+# complejo: complex = 5j
+# tup: tuple = ("0", "1")
+# arr: array = [0, 1]
+# dic: dict = {"key": "value"}
+# bol: bool = True
+# CONSTANTE = 100
+# print(texto, numero, decimal, complejo, tup, arr, dic, bol, CONSTANTE, sep="---", end="------]]")
+# print()
+# palabra = input("Palabra: ")
+# numero = int(input("Numero: "))
+# print(palabra * (CONSTANTE // numero))
 """
 EJERCICIO 2
 
@@ -18,6 +32,27 @@ Pediremos al usuario horas, minutos y segundos de inicio de un evento y su durac
 Calcularemos la hora exacta a la que termina el evento
 """
 
+# horas = int(input("Hora: "))
+# minuts = int(input("Mins: "))
+# seg = int(input("Segs: "))
+# duracion = int(input("Duración: "))
+#
+# todo_segundos = (horas*60*60)+(minuts*60)+seg
+# total = todo_segundos + duracion
+# print("Total seg: " + str(total))
+# total_min = total / 60 / 60
+# print(str(total_min))
+
+
+hora = int(input("Hora: "))
+minu = int(input("Minutos: "))
+dura = int(input("Duración: "))
+
+tiempo_tot_minutos = (hora * 60) + minu + dura
+hora_fin_minutos = tiempo_tot_minutos // 60
+horas_finales = hora_fin_minutos % 24
+minutosFinal = tiempo_tot_minutos % 60
+print(str(horas_finales), str(minutosFinal), sep=":")
 
 """
 EJERCICIO 3
@@ -27,3 +62,23 @@ Comprobaremos de forma separada cada uno de ellos, indicandole al usuario si son
 Finalmente, si ambos son correctos, guardaremos en una variable boolean el resultado y se lo mostraremos por pantalla.
 
 """
+NOMBRE = "Eric"
+USUARIO = "eep"
+nom = input("Nombre: ")
+user = input("Usuario: ")
+
+if NOMBRE == nom:
+    print("Nombre correcto")
+else:
+    print("Usuario correcto: ")
+
+if USUARIO == user:
+    print("Usuario correcto")
+else:
+    print("Usuario incorrecto")
+
+is_both_correct = False
+if NOMBRE == nom and USUARIO == user:
+    is_both_correct = True
+
+print("Son correctos ambos: " + str(is_both_correct))
