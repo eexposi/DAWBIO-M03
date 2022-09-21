615b475db10af3564303de33e5ce4acf48cc4032
Éric <eexposi1@xtec.cat>
2022-09-21 07:46:04 +0200

    Clase 3 corregida

--- Clases/UF1/NF1/ejercicios_clase_3_solucionados.py
+++ Clases/UF1/NF1/ejercicios_clase_3_solucionados.py
@@ -112,27 +112,23 @@ else:
 print("El tramo según tu salario es el " + tramo)
 
 print("\nEJERCICIO 2.2:")
-acumulado = 0
-if 0 <= salario <= 12450:
-    tramo = "1"
-    acumulado += salario * 0.19
-elif 12450 < salario <= 20200:
-    tramo = "2"
-    acumulado += ((salario - 12450) * 0.24) + (12450 * 0.19)
-elif 20200 < salario <= 35200:
-    tramo = "3"
-    acumulado += ((salario - 20200) * 0.30) + (20200 * 0.24)
-elif 35200 < salario <= 60000:
-    tramo = "4"
-    acumulado += ((salario - 35200) * 0.37) + (35200 * 0.30)
-elif 60000 < salario <= 300000:
-    tramo = "5"
-    acumulado += ((salario - 60000) * 0.45) + (60000 * 0.37)
-elif salario > 300000:
-    tramo = "6"
-    acumulado += ((salario - 300000) * 0.47) + (300000 * 0.45)
-else:
-    tramo = "ERROR"
-    acumulado = "ERROR"
-    print("No se encuentra dentro de los baremos")
-print("El tramo aplicable es el: " + tramo + " y has de pagar: " + str(acumulado) + "€")
+if salario > 300000:
+    # Tramo 6
+    impuestos = (12450 * 0.19) + (7749 * 0.24) + (14999 * 0.30) + (24599 * 0.37) + (239999 * 0.45) + (salario - 300001) * 0.47
+elif 300000 >= salario > 60000:
+    # Tramo 5
+    impuestos = (12450 * 0.19) + (7749 * 0.24) + (14999 * 0.30) + (24599 * 0.37) + (salario - 60001) * 0.45
+elif 60000 >= salario > 35200:
+    # Tramo 4
+    impuestos = (12450 * 0.19) + (7749 * 0.24) + (14999 * 0.30) + (salario - 35201) * 0.37
+elif 35200 >= salario > 20200:
+    # Tramo 3
+    impuestos = (12450 * 0.19) + (7749 * 0.24) + (salario - 20201) * 0.30
+elif 20200 >= salario > 12450:
+    # Tramo 2
+    impuestos = 12450 * 0.19 + (salario - 12450) * 0.24
+elif 12450 > salario >= 0:
+    # Tramo 1
+    impuestos = salario * 0.19
+
+print("El tramo aplicable es el: " + str(tramo) + " y has de pagar: " + str(impuestos) + "€")
