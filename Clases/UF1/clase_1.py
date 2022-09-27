2e4b00dd0f02cc468dab620977272970c98bac45
eexposi <113377340+eexposi@users.noreply.github.com>
2022-09-27 18:49:30 +0200

    ok

---
+++ Clases/UF1/clase_1.py
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+""" """ #Tipos de variable
+
+texto: str = "Hola soy un texto"
+=======
+# Tipos de variable
+texto: str = "Hola, soy un texto largo y tedioso"
+>>>>>>> 4e600db36b4ca740c56d96ca3fea1b6635e9f8ea
+numeros: int = 99999
+decimales: float = 0.25
+
+# complejos: complex = 1j
+# tupla = ('abcd', 786, 2.23, 'john', 70.2)
+# diccionario = {'name': 'eric', 'edad': 99, 'departamento': 'informatica'}
+
+print(texto)
+print(numeros)
+print(decimales)
+
+<<<<<<< HEAD
+""" #Un string es un conjunto de caracteres, y podemos acceder a cada uno de ellos.
+=======
+# Un string es un conjunto de caracteres, y podemos acceder a cada uno de ellos.
+>>>>>>> 4e600db36b4ca740c56d96ca3fea1b6635e9f8ea
+print(texto[1])
+
+# Justo como en las tuplas
+print(tupla[1])
+
+# Podemos operar con los tipos basicos
+
+t1 = "hola"
+t2 = " que tal?"
+print(t1 + t2)  # Como vemos, se concatenan las variables
+
+n1 = 5
+n2 = 20
+print(n1 + n2)  # en cambio los numeros se suman
+
+nombre = input("Indique su nombre: ")
+print("Hola " + nombre)
+
+# Podemos añadir texto, concatenando
+# nombre += " Hola!!!!"
+print(nombre)
+
+apellido = input("Indique su apellido: ")
+
+nombre_completo = nombre + " " + apellido
+<<<<<<< HEAD
+print(nombre_completo) """
+ """ """
+=======
+print(nombre_completo)
+
+>>>>>>> 4e600db36b4ca740c56d96ca3fea1b6635e9f8ea
