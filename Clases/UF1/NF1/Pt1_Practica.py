6136a5e9a0e09044621b79b85a5e0989157554fb
expos <eexposi1@xtec.cat>
2022-10-13 13:38:00 +0200

    pt1

---
+++ Clases/UF1/NF1/Pt1_Practica.py
@@ -0,0 +1,16 @@
+"""
+    1. (1p)Elaborar un programa que realice la conversión de cm. a pulgadas. Donde 1cm = 0.39737 pulgadas.
+    2. (1p)Obtener la edad de una persona en meses, si se introduce su edad en años y meses. Ejemplo: Introducimos 3 años 4 meses debe mostrar: 40 meses.
+    3. (4p)Los alumnos  de 1º de DAWBIO realizaran un viaje a Port Aventura  a final de curso, el precio de la entrada general es de 45€. Al grupo se le aplicará un descuento dependiendo del número de alumnos :
+    • Si alumnos > 50 				30%
+    • Si alumnos entre 20  y 50  	           20%
+    • Si alumnos entre 10 y 20 € 	           10%
+    • Si alumnos es <  10 € 			  0%
+    • Profesor de programación 	        entrada libre.
+
+Se realizará un descuento adicional del 15% a los alumnos menores de 18 años la cantidad de alumnos menores del grupo se ha de indicar para obtener el precio final.
+
+El programa mostrará el importe total a pagar para los mayores de edad y el importe a pagar para los menores de edad y el importe total del grupo.
+
+NOTA: Se debe resolver con la estructura If anidado(no se puede usar switch)
+"""
