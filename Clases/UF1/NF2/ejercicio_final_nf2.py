35d63e4225e1a0dec4016003ffc5316c68f83803
jcarra16 <jcarra16@xtec.cat>
2022-10-28 17:12:54 +0200

    yeah!

---
+++ Clases/UF1/NF2/ejercicio_final_nf2.py
@@ -0,0 +1,27 @@
+""" EJERCICIO DE REPASO DE DICCIONARIOS----------------------------------------------------------------
+
+Mediante el uso de diccionario/s, vamos a gestionar los datos de alumnos y sus notas.
+IMPORTANTE: para cada alumno, el sistema debe permitir almacenar un número indeterminado de notas.
+
+El programa va a permitir:
+    - Añadir alumno: si el alumno no existe, se añadirá el nombre y la primera nota (0..10 con decimales).
+      Si ya existe, mostrar mensaje de error y volver a preguntar el nombre de nuevo alumno.
+    - Buscar alumno: mostrará las notas del alumno introducido por teclado. Si no existe, mostrar mensaje
+      de error y volver a preguntar.
+    - Añadir nota: para un alumno introducido por teclado, añadirá una nueva nota.
+    - Mostrar media de notas: para un alumno introducido por teclado, mostrará la media de sus notas.
+    - Borrar estudiante: borrará al alumno indicado de nuestra base de datos. Si no existe, mostrar mensaje
+      de error y volver a preguntar el nombre del alumno a borrar.
+
+NOTA: si nos hemos confundido de menú, introduciremos una "X" cuando nos pida el nombre del alumno. El sistema
+volverá al menú principal.
+
+                                            Menú de opciones:
+                                                1 - Añadir alumno
+                                                2 - Buscar alumno
+                                                3 - Añadir nota a alumno
+                                                5 - Mostrar la media de notas de alumno
+                                                7 - Borrar un alumno
+                                                0 - Salir
+                                            Introduce opción:
+--------------------------------------------------------------------------------------------------- """
