8360d80456f10c44635b99d876c464a42f172917
javier <jcarra16@xtec.cat>
2022-10-28 13:43:15 +0200

    A ver si va...

--- Clases/UF1/NF2/ejercicios_clase_7_solucion.py
+++ Clases/UF1/NF2/ejercicios_clase_7_solucion.py
@@ -49,7 +49,7 @@ cesta = {}
 continuar = True
 while continuar:
     item = input("Introduce un artículo: ")
-    precio = float(input("Introduce el precio de " + item + "": ""))
+    precio = float(input("Introduce el precio de " + item + ": "))
     cesta[item] = precio
     continuar = input("¿Quieres añadir artículos a la lista (Si/No)? ") == "Si"
 coste = 0
