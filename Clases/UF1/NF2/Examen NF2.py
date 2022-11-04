feef2b05933a6f594b78fe391c19b6442197d315
javier <jcarra16@xtec.cat>
2022-11-04 17:29:22 +0100

    A ver si va...

--- Clases/UF1/NF2/Examen NF2.py
+++ Clases/UF1/NF2/Examen NF2.py
@@ -1,22 +1,66 @@
 """ EXAMEN NF2 - MATRICES, DICCIONARIOS Y CONTROL DE ERRORES --------------------------------------------------------------
 
-EJERCICIO 1
+-----------------------------------------------------------
+EJERCICIO 1  ----------------------------------------------
+-----------------------------------------------------------
 - Pedir al usuario su nombre y su DNI sin letra.
-- Crear un diccionario con la key el nombre y el value el DNI CON la letra.
+- Crear un diccionario con la key : nombre y el value : DNI con la letra.
 - Mostrar todos los datos en forma de tabla.
 
-Usuarios
----------
-Nombre --> XXXXXXXX-X
+                    Usuarios
+                    ---------
+                    Nombre --> XXXXXXXX-X
 
 
-EJERCICIO 2
--
+-----------------------------------------------------------
+EJERCICIO 2: CALCULADORA DE DIVISIÓN ----------------------
+-----------------------------------------------------------
+- Pedir al usuario 2 números.
+- While hasta que le introduzcamos 'X' en el primer valor
+- Realizar la división entre ellos, gestionando todos los posibles errores y mostrando por pantalla el error.
 
 
 
+-----------------------------------------------------------
+EJERCICIO 3: HUNDIR LA FLOTA ------------------------------
+-----------------------------------------------------------
+El código va a crear un tablero de 4x4 y va a colocar 2 barcos en él. El código debe permitir, fácilmente, cambiar
+la posición de los barcos antes de lanzar el programa.
+
+# Antes de empezar a jugar:
+- Creamos un tablero de 4x4 vacío ('A')
+- Colocamos 2 barcos (posición con 'B'): 2 celdas y 3 celdas (horizontal o vertical pero que no solapen)
+
+# Al iniciar el programa...
+- Mostrar el menú de juego (diccionario o lista) y tablero inicial con los barcos:
+        1 - Jugar
+        0 - Salir
+- En random, hacemos tiradas:
+    - devolver posición de la tirada
+    - devolver agua (cambiar por 'X') o tocado (cambiar por 'T')
+    - devolver estado del tablero
+    - Esperar 'ENTER' para siguiente tirada
+- Al completar 6 tiradas, deberemos devolver un mensaje según si hemos conseguido hundir todos los barcos o no
 
 
+EJEMPLO DEL 1ER TURNO ----------------------------------------------------
+-  Mostramos el tablero al empezar el juego:
+           0 1 2 3
+         0 A B B A
+         1 A A A B
+         2 A A A B
+         3 A A A B
 
+- Vemos las posiciones del disparo:
+    Se dispara en la posicion x: 0 y: 1
+    Tocado!
+
+- Mostramos el tablero con las modificaciones:
+           0 1 2 3
+         0 A T B A
+         1 A A A B
+         2 A A A B
+         3 A A A B
+- Esperamos al siguiente turno:   "Pulsa ENTER para continuar..."
 
 --------------------------------------------------------------------------------------------------- """
