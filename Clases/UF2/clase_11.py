98904539f0fc7a75644149ce7c1bfb3d790b92eb
ericexposito <eexposi1@xtec.cat>
2022-11-23 12:32:30 +0100

    Clase 10

---
+++ Clases/UF2/clase_11.py
@@ -0,0 +1,12 @@
+"""Metodos 2"""
+
+"""Palabra: Global
+
+La variable con esta indicación a la que se accedera sera la del resto del programa."""
+
+
+
+"""
+Recursividad
+
+Un metodo que se llama a si mismo. Recordar de poner alguna condicion de salida o sera infinito"""
